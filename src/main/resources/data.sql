dec4a4e67497e3f5839141154caf04192c39c618
toranzam <nbc05829@gmail.com>
2024-01-29 21:13:38 +0900

    ADD 유저네임 2인 기본계정 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
-insert into account values (1, '$2a$10$LkT1p3I01vOSs5PJeQZwPeJlXPJMbWcdfWFOXLsA4ZcdQClObFWgi', '1')
+insert into account values (10001, '$2a$10$LkT1p3I01vOSs5PJeQZwPeJlXPJMbWcdfWFOXLsA4ZcdQClObFWgi', '1');
+insert into account values (10002, '$2a$10$DJRmR6zqZ42HOhouOSxhweDSxwZduPh0wJnxbhI7tCpO12pGVKZC.', '2');
